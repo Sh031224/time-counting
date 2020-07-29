2c84c3a16ba07637f0c5eea81621cfa42e4ba9ec
Sh031224 <1cktmdgh2@gmail.com>
2020-07-29 10:02:42 +0900

    [ADD] time after

--- src/index.ts
+++ src/index.ts
@@ -4,7 +4,7 @@ interface TimeCountingOption {
   objectTime?: Date | string | number;
   lang?: "ko" | "en";
   calculate?: {
-    JustBefore?: number;
+    justNow?: number;
     second?: number;
     minute?: number;
     hour?: number;
@@ -21,7 +21,7 @@ export default (time: TimeCountingDate, option?: TimeCountingOption) => {
   let lang = "en";
 
   let calculate = {
-    JustBefore: 20,
+    justNow: 20,
     second: 60,
     minute: 60,
     hour: 24,
@@ -31,7 +31,8 @@ export default (time: TimeCountingDate, option?: TimeCountingOption) => {
   };
 
   let message = {
-    justBefore: "just now",
+    justAfter: "just after",
+    justNow: "just now",
     second: "second",
     minute: "minute",
     hour: "hour",
@@ -51,69 +52,128 @@ export default (time: TimeCountingDate, option?: TimeCountingOption) => {
     if (option.lang === "ko") {
       lang = "ko";
       message = {
-        justBefore: "방금 전",
-        second: "초 전",
-        minute: "분 전",
-        hour: "시간 전",
-        day: "일 전",
-        week: "주 전",
-        month: "개월 전",
-        year: "년 전"
+        justAfter: "조금",
+        justNow: "방금",
+        second: "초",
+        minute: "분",
+        hour: "시간",
+        day: "일",
+        week: "주",
+        month: "개월",
+        year: "년"
       };
     }
   }
-  const calc = today.valueOf() - date.valueOf();
+  let calc = today.valueOf() - date.valueOf();
 
-  if (calc < calculate.JustBefore * 1000) {
-    return `${message.justBefore}`;
-  } else if (calc < calculate.second * 1000) {
-    const time = Math.ceil(calc / 1000);
-    return `${time}${message.second}` + lang === "en" && time > 1
-      ? "s ago"
-      : lang === "en"
-      ? " ago"
-      : "";
-  } else if (calc < 60 * calculate.minute * 1000) {
-    const time = Math.ceil(calc / 1000 / 60);
-    return `${time}${message.minute}` + lang === "en" && time > 1
-      ? "s ago"
-      : lang === "en"
-      ? " ago"
-      : "";
-  } else if (calc < 60 * 60 * calculate.hour * 1000) {
-    const time = Math.ceil(calc / 1000 / 60 / 60);
-    return `${time}${message.hour}` + lang === "en" && time > 1
-      ? "s ago"
-      : lang === "en"
-      ? " ago"
-      : "";
-  } else if (calc < 60 * 60 * 24 * calculate.day * 1000) {
-    const time = Math.ceil(calc / 1000 / 60 / 60 / 24);
-    return `${time}${message.day}` + lang === "en" && time > 1
-      ? "s ago"
-      : lang === "en"
-      ? " ago"
-      : "";
-  } else if (calc < 60 * 60 * 24 * 7 * calculate.week * 1000) {
-    const time = Math.ceil(calc / 1000 / 60 / 60 / 24 / 7);
-    return `${time}${message.week}` + lang === "en" && time > 1
-      ? "s ago"
-      : lang === "en"
-      ? " ago"
-      : "";
-  } else if (calc < 60 * 60 * 24 * 7 * 4 * calculate.month * 1000) {
-    const time = Math.ceil(calc / 1000 / 60 / 60 / 24 / 7 / 4);
-    return `${time}${message.month}` + lang === "en" && time > 1
-      ? "s ago"
-      : lang === "en"
-      ? " ago"
-      : "";
+  if (calc < 0) {
+    calc *= -1;
+    if (calc < calculate.justNow * 1000) {
+      const msg = lang === "en" ? "" : " 후";
+      return `${message.justNow}${msg}`;
+    } else if (calc < calculate.second * 1000) {
+      const time = Math.floor(calc / 1000);
+      const msg =
+        lang === "en" && time > 1
+          ? "s after"
+          : lang === "en"
+          ? " after"
+          : " 후";
+      return `${time} ${message.second}${msg}`;
+    } else if (calc < 60 * calculate.minute * 1000) {
+      const time = Math.floor(calc / 1000 / 60);
+      const msg =
+        lang === "en" && time > 1
+          ? "s after"
+          : lang === "en"
+          ? " after"
+          : " 후";
+      return `${time} ${message.minute}${msg}`;
+    } else if (calc < 60 * 60 * calculate.hour * 1000) {
+      const time = Math.floor(calc / 1000 / 60 / 60);
+      const msg =
+        lang === "en" && time > 1
+          ? "s after"
+          : lang === "en"
+          ? " after"
+          : " 후";
+      return `${time} ${message.hour}${msg}`;
+    } else if (calc < 60 * 60 * 24 * calculate.day * 1000) {
+      const time = Math.floor(calc / 1000 / 60 / 60 / 24);
+      const msg =
+        lang === "en" && time > 1
+          ? "s after"
+          : lang === "en"
+          ? " after"
+          : " 후";
+      return `${time} ${message.day}${msg}`;
+    } else if (calc < 60 * 60 * 24 * 7 * calculate.week * 1000) {
+      const time = Math.floor(calc / 1000 / 60 / 60 / 24 / 7);
+      const msg =
+        lang === "en" && time > 1
+          ? "s after"
+          : lang === "en"
+          ? " after"
+          : " 후";
+      return `${time} ${message.week}${msg}`;
+    } else if (calc < 60 * 60 * 24 * 7 * 4 * calculate.month * 1000) {
+      const time = Math.floor(calc / 1000 / 60 / 60 / 24 / 7 / 4);
+      const msg =
+        lang === "en" && time > 1
+          ? "s after"
+          : lang === "en"
+          ? " after"
+          : " 후";
+      return `${time} ${message.month}${msg}`;
+    } else {
+      const time = Math.floor(calc / 1000 / 60 / 60 / 24 / 7 / 4 / 12);
+      const msg =
+        lang === "en" && time > 1
+          ? "s after"
+          : lang === "en"
+          ? " after"
+          : " 후";
+      return `${time} ${message.year}${msg}`;
+    }
   } else {
-    const time = Math.ceil(calc / 1000 / 60 / 60 / 24 / 7 / 4 / 12);
-    return `${time}${message.year}` + lang === "en" && time > 1
-      ? "s ago"
-      : lang === "en"
-      ? " ago"
-      : "";
+    if (calc < calculate.justNow * 1000) {
+      const msg = lang === "en" ? "" : " 전";
+      return `${message.justNow}${msg}`;
+    } else if (calc < calculate.second * 1000) {
+      const time = Math.floor(calc / 1000);
+      const msg =
+        lang === "en" && time > 1 ? "s ago" : lang === "en" ? " ago" : "";
+      return `${time} ${message.second}${msg}`;
+    } else if (calc < 60 * calculate.minute * 1000) {
+      const time = Math.floor(calc / 1000 / 60);
+      const msg =
+        lang === "en" && time > 1 ? "s ago" : lang === "en" ? " ago" : "";
+      return `${time} ${message.minute}${msg}`;
+    } else if (calc < 60 * 60 * calculate.hour * 1000) {
+      const time = Math.floor(calc / 1000 / 60 / 60);
+      const msg =
+        lang === "en" && time > 1 ? "s ago" : lang === "en" ? " ago" : "";
+      return `${time} ${message.hour}${msg}`;
+    } else if (calc < 60 * 60 * 24 * calculate.day * 1000) {
+      const time = Math.floor(calc / 1000 / 60 / 60 / 24);
+      const msg =
+        lang === "en" && time > 1 ? "s ago" : lang === "en" ? " ago" : "";
+      return `${time} ${message.day}${msg}`;
+    } else if (calc < 60 * 60 * 24 * 7 * calculate.week * 1000) {
+      const time = Math.floor(calc / 1000 / 60 / 60 / 24 / 7);
+      const msg =
+        lang === "en" && time > 1 ? "s ago" : lang === "en" ? " ago" : "";
+      return `${time} ${message.week}${msg}`;
+    } else if (calc < 60 * 60 * 24 * 7 * 4 * calculate.month * 1000) {
+      const time = Math.floor(calc / 1000 / 60 / 60 / 24 / 7 / 4);
+      const msg =
+        lang === "en" && time > 1 ? "s ago" : lang === "en" ? " ago" : "";
+      return `${time} ${message.month}${msg}`;
+    } else {
+      const time = Math.floor(calc / 1000 / 60 / 60 / 24 / 7 / 4 / 12);
+      const msg =
+        lang === "en" && time > 1 ? "s ago" : lang === "en" ? " ago" : "";
+      return `${time} ${message.year}${msg}`;
+    }
   }
 };
